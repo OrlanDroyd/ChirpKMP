d3f4fd1e3d0cdf4d6dd4623c1dd2ae40e7ee96b1
OrlanDroyd <orlandroyd@gmail.com>
2026-08-16 20:36:36 +0100

    add resendVerificationEmail to auth service interface and implementation; update success layout to display resend verification message and handle resend event; enhance RegisterSuccessViewModel to manage resend verification logic with error handling; update string resources with resend verification messages.

--- feature/auth/presentation/src/commonMain/kotlin/com/github/orlandroyd/auth/presentation/register_success/RegisterSuccessScreen.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/github/orlandroyd/auth/presentation/register_success/RegisterSuccessScreen.kt
@@ -17,7 +17,7 @@ import com.github.orlandroyd.core.designsystem.components.brand.ChirpSuccessIcon
 import com.github.orlandroyd.core.designsystem.components.buttons.ChirpButton
 import com.github.orlandroyd.core.designsystem.components.buttons.ChirpButtonStyle
 import com.github.orlandroyd.core.designsystem.components.layouts.ChirpAdaptiveResultLayout
-import com.github.orlandroyd.core.designsystem.components.layouts.ChirpSimpleSuccessLayout
+import com.github.orlandroyd.core.designsystem.components.layouts.ChirpSimpleResultLayout
 import com.github.orlandroyd.core.designsystem.components.layouts.ChirpSnackbarScaffold
 import com.github.orlandroyd.core.designsystem.theme.ChirpTheme
 import com.github.orlandroyd.core.presentation.util.ObserveAsEvents
@@ -63,7 +63,7 @@ fun RegisterSuccessScreen(
         snackbarHostState = snackbarHostState
     ) {
         ChirpAdaptiveResultLayout {
-            ChirpSimpleSuccessLayout(
+            ChirpSimpleResultLayout(
                 title = stringResource(Res.string.account_successfully_created),
                 description = stringResource(
                     Res.string.verification_email_sent_to_x,
